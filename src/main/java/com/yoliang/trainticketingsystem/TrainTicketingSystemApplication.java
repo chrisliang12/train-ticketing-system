fa8f0db0f70f4df2c3a5e67524adc8d609889650
yoliang <yoliang@ucsd.edu>
2023-05-04 23:38:16 -0700

    initialization

---
+++ src/main/java/com/yoliang/trainticketingsystem/TrainTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yoliang.trainticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketingSystemApplication.class, args);
+    }
+
+}
