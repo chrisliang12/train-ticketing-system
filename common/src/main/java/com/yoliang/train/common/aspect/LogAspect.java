fb02f26eae5b8e534c165352039afc883ef52283
yoliang <yoliang@ucsd.edu>
2023-05-05 15:43:38 -0700

    add common module which is used to place common classes

--- common/src/main/java/com/yoliang/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/yoliang/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.yoliang.train.member.aspect;
+package com.yoliang.train.common.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
