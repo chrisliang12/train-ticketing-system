a834deb15cd1afcaf8207971aa2b11513c40707d
yoliang <yoliang@ucsd.edu>
2023-05-05 00:16:32 -0700

    modified initialization log

--- member/src/main/java/com/yoliang/train/member/config/MemberApplication.java
+++ member/src/main/java/com/yoliang/train/member/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.yoliang.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.yoliang")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Program started successfully...");
+        LOG.info("Addr: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
